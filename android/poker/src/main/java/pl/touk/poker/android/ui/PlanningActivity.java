12817fb16bcd0168c87e1cd765814bbc17048b0b
bwj <bwj@touk.pl>
2014-07-04 13:07:21 +0200

    added actual estimation

--- android/poker/src/main/java/pl/touk/poker/android/ui/PlanningActivity.java
+++ android/poker/src/main/java/pl/touk/poker/android/ui/PlanningActivity.java
@@ -2,12 +2,22 @@ package pl.touk.poker.android.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import butterknife.OnClick;
 import pl.touk.poker.android.R;
 import pl.touk.poker.android.config.dagger.Injector;
+import pl.touk.poker.android.network.EmptyResponse;
+import pl.touk.poker.android.network.EstimateRequest;
 import pl.touk.poker.android.network.ServerClient;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 import javax.inject.Inject;
 
@@ -17,6 +27,9 @@ public class PlanningActivity extends Activity {
 
     @InjectView(R.id.sessionId)
     TextView sessionIdView;
+
+    @InjectView(R.id.estimateSpinner)
+    Spinner estimateSpinner;
 
     String userId;
     String sessionId;
@@ -34,11 +47,37 @@ public class PlanningActivity extends Activity {
     protected void onResume() {
         super.onResume();
         sessionIdView.setText(sessionId);
+        String[] estimates = {"1", "2", "3", "5", "8"};
+        estimateSpinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, estimates));
+    }
+
+    @OnClick(R.id.sendButton)
+    public void onSend(View view) {
+        EstimateRequest req = new EstimateRequest();
+        req.setUserId(userId);
+        req.setEstimate(estimateSpinner.getSelectedItem().toString());
+        client.getApi().estimate(req, sessionId, new ToastResponse());
     }
 
     private void initCredentials() {
         this.userId = getIntent().getStringExtra("userId");
         this.sessionId = getIntent().getStringExtra("sessionId");
+    }
+
+    private class ToastResponse implements Callback<EmptyResponse> {
+        @Override
+        public void success(EmptyResponse emptyResponse, Response response) {
+            toast("Success");
+        }
+
+        @Override
+        public void failure(RetrofitError retrofitError) {
+            toast("Failure");
+        }
+
+        protected void toast(String message) {
+            Toast.makeText(PlanningActivity.this, message, Toast.LENGTH_LONG).show();
+        }
     }
 
 }
