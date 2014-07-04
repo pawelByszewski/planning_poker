6a08434af34a06e4cb7af7071c57f9512ea2fdb7
bwj <bwj@touk.pl>
2014-07-04 11:11:48 +0200

    added simple onClick handling for join session button

--- android/poker/src/main/java/pl/touk/poker/android/ui/ComponentsActivity.java
+++ android/poker/src/main/java/pl/touk/poker/android/ui/ComponentsActivity.java
@@ -2,10 +2,13 @@ package pl.touk.poker.android.ui;
 
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import pl.touk.poker.android.R;
@@ -30,6 +33,27 @@ public class ComponentsActivity extends Activity {
         ButterKnife.inject(this);
     }
 
+    public void onJoinSession(View view) {
+        showAlert();
+    }
+
+    public void showAlert() {
+        new AlertDialog.Builder(this)
+                .setTitle("Ugabuga!")
+                .setMessage("onClock works!")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // continue with delete
+                    }
+                })
+                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // do nothing
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
