d3912344d143b4b57b956c6515ea289ff96fd09f
bwj <bwj@touk.pl>
2014-07-04 11:02:16 +0200

    gui for joining session added

--- android/poker/src/main/java/pl/touk/poker/android/ui/ComponentsActivity.java
+++ android/poker/src/main/java/pl/touk/poker/android/ui/ComponentsActivity.java
@@ -28,49 +28,16 @@ public class ComponentsActivity extends Activity {
         setContentView(R.layout.components_view);
 //        Injector.inject(this);
         ButterKnife.inject(this);
-
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.fragmentPlaceholder, new ComponentsFragment(), "blur").commit();
-        }
-
-//        drawerLayout.setDrawerShadow(R.drawable.drawer_shadow, Gravity.START);
-//
-//        actionBarDrawerToggle = new ActionBarDrawerToggle(
-//                this,                  /* host Activity */
-//                drawerLayout,         /* DrawerLayout object */
-//                R.drawable.ic_drawer,  /* nav drawer icon to replace 'Up' caret */
-//                R.string.drawer_open,  /* "open drawer" description */
-//                R.string.drawer_close  /* "close drawer" description */
-//        ) {
-//
-//            public void onDrawerClosed(View view) {
-//                getActionBar().setTitle(ComponentsActivity.this.getTitle());
-//            }
-//
-//            public void onDrawerOpened(View drawerView) {
-//                getActionBar().setTitle(ComponentsActivity.this.getTitle());
-//            }
-//        };
-//
-//        drawerLayout.setDrawerListener(actionBarDrawerToggle);
-//        getActionBar().setDisplayHomeAsUpEnabled(true);
-//        getActionBar().setHomeButtonEnabled(true);
-//        actionBarDrawerToggle.syncState();
     }
 
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-//        actionBarDrawerToggle.syncState();
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-//        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
-//            return true;
-//        }
         return false;
     }
 }
