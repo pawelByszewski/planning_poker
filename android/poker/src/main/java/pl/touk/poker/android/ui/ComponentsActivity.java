5e714987e51a4516193c880a96de5232e98e9799
bwj <bwj@touk.pl>
2014-07-04 12:16:06 +0200

    quickfix after merge

--- android/poker/src/main/java/pl/touk/poker/android/ui/ComponentsActivity.java
+++ android/poker/src/main/java/pl/touk/poker/android/ui/ComponentsActivity.java
@@ -47,10 +47,6 @@ public class ComponentsActivity extends Activity {
     public void onJoinSession(View view) {
         Request request = new Request();
         request.setName(usernameInput.getText().toString());
-        ServerClient.getApi().joinSession(request, sessionInput.getText().toString(), new SessionJoined());
-        request.setName("Adam");
-        //ServerClient.getApi().joinSession(request, sessionInput.getText().toString(), new SessionJoined());
-//        showAlert();
         client.getApi().joinSession(request, sessionInput.getText().toString(), new SessionJoined());
     }
 
